cf9f9d6506dda07daf678b20292cb5d13f25316b
Brandon lee <leevash203@gmail.com>
2023-04-24 00:12:22 +0200

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static.
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-	Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
-sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-/etc/init.d/nginx restart
+# sets up your web servers for the deployment of web_static
+
+#install nginx
+apt-get update
+apt-get install -y nginx
+
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Release test" >> /data/web_static/releases/test/index.html
+# Create link
+ln -sfn /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/ 
+
+#static
+
+printf %s "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        add_header X-Served-By $HOSTNAME;
+        root /var/www/html;
+        index index.html;
+        
+        location /hbnb_static {
+            alias /data/web_static/current;
+            index index.html index.htm;
+        }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
