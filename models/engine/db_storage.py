e55a4780be9cd9e657f9613de77fe425e6843542
brandon29 <leevash203@gmail.com>
2023-04-23 14:39:02 -0700

    my commit

---
+++ models/engine/db_storage.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+# This is the file storage class for AirBnB
+
+import os
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.scoping import scoped_session
+from sqlalchemy import create_engine
+from models import *
+
+
+class DBStorage:
+    __engine = None
+    __session = None
+    valid_classes = ["User", "State", "City", "Amenity", "Place", "Review"]
+
+    def __init__(self):
+        self.__engine = create_engine("mysql+mysqldb://" +
+                                      os.environ['HBNB_MYSQL_USER'] +
+                                      ":" + os.environ['HBNB_MYSQL_PWD'] +
+                                      "@" + os.environ['HBNB_MYSQL_HOST'] +
+                                      ":3306/" +
+                                      os.environ['HBNB_MYSQL_DB'])
+
+        try:
+            if os.environ['HBNB_MYSQL_ENV'] == "test":
+                Base.metadata.drop_all(self.__engine)
+        except KeyError:
+            pass
+
+    def all(self, cls=None):
+        storage = {}
+        if cls is None:
+            for cls_name in self.valid_classes:
+                for instance in self.__session.query(eval(cls_name)):
+                    storage[instance.id] = instance
+        else:
+            if cls not in self.valid_classes:
+                return
+            for instance in self.__session.query(eval(cls)):
+                storage[instance.id] = instance
+
+        return storage
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        try:
+            self.__session.commit()
+        except:
+            self.__session.rollback()
+            raise
+
---
+++ models/engine/file_storage.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+A script that has the FileStorage class
+"""
+
+import json
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from hashlib import md5
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
+
+class FileStorage:
+    """serializes instances to a JSON file & deserializes back to instances"""
+
+    # string - path to the JSON file
+    __file_path = "file.json"
+    # dictionary - empty but will store all objects by <class name>.id
+    __objects = {}
+
+    def all(self, cls=None):
+        """returns the dictionary __objects"""
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
+
